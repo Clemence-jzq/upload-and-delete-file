505d88ddc051a8e5f146f5c7152766040143eb62
Clemence-jzq <zheqin.jiang@gmail.com>
2023-04-06 22:46:14 +0200

    show all data

---
+++ list.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config/config.php';
+
+$sql = "SELECT * FROM person";
+if ($sql = mysqli_prepare($con, $sql)) {
+    mysqli_stmt_execute($sql);
+}
+$res = mysqli_stmt_get_result($sql);
+
+if (mysqli_num_rows($res) > 0) {
+    while ($row = mysqli_fetch_assoc($res)) {
+        echo
+            "<tr>
+                <td>$row[id]</td>
+                <td>$row[name]</td>
+                <td>$row[age]</td>
+                <td>$row[file]</td>
+            </tr>";
+    }
+}
